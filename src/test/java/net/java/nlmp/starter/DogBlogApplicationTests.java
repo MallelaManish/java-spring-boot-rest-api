7ab9c5ecfbb6c7255922d2e846169fd93652be56
mallela manish <61269709+MallelaManish@users.noreply.github.com>
2023-12-18 16:05:06 +0530

    first commit

---
+++ src/test/java/net/java/nlmp/starter/DogBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package net.java.nlmp.starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
